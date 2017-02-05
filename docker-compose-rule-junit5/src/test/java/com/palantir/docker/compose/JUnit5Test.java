c207d4403fbb5785987a9b43ed20bf64c6752825
James Neale <jneale@palantir.com>
2017-02-05 14:18:58 +0000

    Simple JUnit5 test

---
+++ docker-compose-rule-junit5/src/test/java/com/palantir/docker/compose/JUnit5Test.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2016 Palantir Technologies, Inc. All rights reserved.
+ */
+
+package com.palantir.docker.compose;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class JUnit5Test {
+
+    @Test
+    public void test() {
+        Assertions.assertEquals(1 + 1, 2);
+    }
+
+}
